399839d62c69ea59539350e151e26757e7c689fd
Renat Abaidulin <ares566@ya.ru>
2015-07-22 14:18:48 +0300

    New file

---
+++ ARobotic.cpp
@@ -0,0 +1,11 @@
+/*
+  ARobotic.cpp - Library for Arduino robotic devices.
+  Created by Renat Abaidulin, July 22, 2015.
+*/
+
+#include "Arduino.h"
+#include "ARobotic.h"
+
+ARobotic::ARobotic(){
+  
+}
--- ARobotic.h
+++ ARobotic.h
@@ -1,1 +1,24 @@
+/*
+  ARobotic.h - Library for Arduino robotic devices.
+  Created by Renat Abaidulin, July 22, 2015.
+*/
 
+#ifndef arobotic_h
+#define arobotic_h
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+
+class ARobotic
+{
+public:
+	ARobotic();
+
+private:
+	
+};
+#endif
