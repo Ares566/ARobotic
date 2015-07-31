79dc2a82416832c8340325f2ee5a2b75ee6e7024
Renat Abaidulin <ares566@ya.ru>
2015-07-31 15:13:45 +0300

    Добавлены функции работы с моторами и их энкодерами.

--- ARobotic.cpp
+++ ARobotic.cpp
@@ -6,6 +6,173 @@
 
 #include "ARobotic.h"
 
-ARobotic::ARobotic(int mode){
-  
+#define ENL 5           // ШИМ вывод для управления правым двигателем 0 - стоп, подтянуто на землю
+#define ENR 6           // ШИМ вывод для управления левым двигателем  0 - стоп, подтянуто на землю
+#define DIRL 4          // Направление вращение левого двигателя 
+#define DIRR 7          // Направление вращение правого двигателя 
+
+
+ARobotic::ARobotic(int mode,uint16_t ar_width){
+  lw_enc_counter = rw_enc_counter = 0;
+  _ar_width = ar_width;
+
+  pinMode(ENL, OUTPUT);
+  pinMode(ENR, OUTPUT);
+  pinMode(DIRL, OUTPUT);
+  pinMode(DIRR, OUTPUT);
+  //если режим работы с энкодерами
+  if (mode && AR_M_WITH_ENC){
+	  //подключаем энкодеры
+	  pinMode(2, INPUT);   //2 пин отвечает за 0 прерывание
+	  digitalWrite(2, HIGH);       // включаем подтягивающий резистор
+	  attachInterrupt(0, encoderFuncM_L, CHANGE);  // энкодер левого колеса
+
+	  pinMode(3, INPUT);   //3 пин отвечает за 1 прерывание
+	  digitalWrite(3, HIGH);       // включаем подтягивающий резистор
+	  attachInterrupt(1, encoderFuncM_R, CHANGE);  // энкодер правого колеса
+  }
 }
+
+/*
+*  Работа с  моторами
+*  при передаче ненулевых параметров выход из функции только по истечении dest
+*/
+//едем вперед
+void ARobotic::moveForward(uint8_t speed, uint16_t distance){
+	
+	digitalWrite(DIRL,HIGH);
+	digitalWrite(DIRR,HIGH);
+	digitalWrite(ENR,speed);
+	digitalWrite(ENL,speed);
+
+	//обработка distance
+	if (distance){
+		waitingDistance4LM(distance);
+		stopMoving();
+	}
+}
+
+//едем назад
+void ARobotic::moveBackward(uint8_t speed, uint16_t distance){
+
+	digitalWrite(DIRL,LOW);
+	digitalWrite(DIRR,LOW);
+	digitalWrite(ENR,speed);
+	digitalWrite(ENL,speed);
+
+	//обработка distance
+	if (distance){
+		waitingDistance4LM(distance);
+		stopMoving();
+	}
+}
+
+//поворот через левое колесо на определенный градус
+void ARobotic::turnLeft(int degree){
+
+	//поворачиваем вперед...
+	if (degree>0){
+		digitalWrite(DIRR,HIGH);
+	}else
+		digitalWrite(DIRR,LOW);//...или назад
+	
+	//скорость разворота
+	uint8_t speed = 220;
+	//длина дуги
+	uint16_t ArcLen = round(PI * _ar_width * degree / 1800);
+	
+	//если развернуться нужно на мешьше оборота колеса, делаем скорость ниже
+	if (ArcLen < 20){
+		speed = 200;
+	}
+
+	digitalWrite(ENL,0);//левое колесо на месте
+	digitalWrite(ENR,speed);//правое крутим
+	
+	waitingDistance4RM(ArcLen);
+	stopMoving();
+}
+
+//поворот через правое колесо на определенный градус
+void ARobotic::turnRight(int degree){
+	
+	//поворачиваем вперед...
+	if (degree>0){
+		digitalWrite(DIRL,HIGH);
+	}else
+		digitalWrite(DIRL,LOW);//...или назад
+
+	//скорость разворота
+	uint8_t speed = 220;
+	//длина дуги
+	uint16_t ArcLen = round(PI * _ar_width * degree / 1800);
+
+	//если развернуться нужно на мешьше оборота колеса, делаем скорость ниже
+	if (ArcLen < 20){
+		speed = 200;
+	}
+
+	digitalWrite(ENL,speed);
+	digitalWrite(ENR,0);
+
+	waitingDistance4LM(ArcLen);
+	stopMoving();
+}
+
+//останов обоих моторов
+void ARobotic::stopMoving(){
+	digitalWrite(ENR,0);
+	digitalWrite(ENL,0);
+}
+
+//ждем пока левый мотор проедет определенную дистанцию
+void ARobotic::waitingDistance4LM(uint16_t distance){
+	uint16_t iCurDistance = getLeftWheelDistance();
+	while((iCurDistance + distance) > getLeftWheelDistance()){}
+}
+
+//ждем пока правый мотор проедет определенную дистанцию
+void ARobotic::waitingDistance4RM(uint16_t distance){
+	uint16_t iCurDistance = getRightWheelDistance();
+	while((iCurDistance + distance) > getRightWheelDistance()){}
+}
+/*
+*  Работа с энкодерами моторов
+*  подсчитываем пройденное расстояние
+*/
+//функция вызываемая прерыванием для подсчета оборотов левого колеса
+void  ARobotic::encoderFuncM_L() {
+  if (digitalRead(2) == HIGH) {
+    lw_enc_counter++;
+  } 
+}
+//функция вызываемая прерыванием для подсчета оборотов левого колеса
+void ARobotic::encoderFuncM_R() {
+  if (digitalRead(3) == HIGH) {
+    rw_enc_counter++;
+  } 
+
+}
+
+//сколько сантиметров проехало левое колесо
+uint16_t ARobotic::getLeftWheelDistance(){
+	//один оборот двигателя равен 20 импульсам диаметр колеса в мм
+	return round( lw_enc_counter * PI*AR_WHEEL_D / 200 );
+}
+
+//сколько сантиметров проехало правое колесо
+uint16_t ARobotic::getRightWheelDistance(){
+	//один оборот двигателя равен 20 импульсам диаметр колеса в мм
+	return round( rw_enc_counter * PI*AR_WHEEL_D / 200 );
+}
+
+//обнуление пройденного расстояния левого колеса
+void ARobotic::resetLeftWheelDistance(){
+	lw_enc_counter = 0;
+}
+
+//обнуление пройденного расстояния правого колеса
+void ARobotic::resetRightWheelDistance(){
+	rw_enc_counter = 0;
+}
+
--- ARobotic.h
+++ ARobotic.h
@@ -7,18 +7,54 @@
 #define arobotic_h
 
 #if ARDUINO >= 100
- #include "Arduino.h"
+ #include <Arduino.h>
 #else
- #include "WProgram.h"
+ #include <WProgram.h>
+#endif
+//работа с экраном по умолчанию выключена т.к. библиотека занимает много места
+#if defined(AR_USE_LCD)
+  #include <OLED_I2C.h>
 #endif
 
+#define AR_M_WITH_ENC 1//режим работы с энкодерами 
+#define AR_M_WITH_NRF 2//режим работы с nrf24l01 
+
+#define AR_WHEEL_D 65 //диаметр колеса в мм
 
 class ARobotic
 {
 public:
-	ARobotic(int mode);//режим работы: с энкодерами или без
+	/*
+	* 	mode - режим работы: с энкодерами или без(???) может  define-ми регулировать режимы работы
+	*	ar_width - ширина модели в мм
+	*/
+	ARobotic(int mode,uint16_t ar_width);
+
+	void moveForward(uint8_t speed, uint16_t distance);//едем вперед
+	void moveBackward(uint8_t speed, uint16_t distance);//едем назад
+	void turnLeft(int degree);//поворот через левое колесо на определенный градус
+	void turnRight(int degree);//поворот через правое колесо на определенный градус
+	void stopMoving();//останов обоих моторов
+	void waitingDistance4LM(uint16_t distance);//ждем пока левый мотор проедет определенную дистанцию
+	void waitingDistance4RM(uint16_t distance);//ждем пока правый мотор проедет определенную дистанцию
+
+	//публичные функции работы с энкодерами колес
+	uint16_t getLeftWheelDistance();//пройденное расстояние левого колеса
+	uint16_t getRightWheelDistance();//пройденное расстояние правого колеса
+	void resetLeftWheelDistance();//обнуление пройденного расстояния левого колеса
+	void resetRightWheelDistance();//обнуление пройденного расстояния правого колеса
+
+	//функции для работы с экраном
+	#if defined(AR_USE_LCD)
+	void lcdPrint(char *msg,int row,int line);//печать сообщения на экране
+	#endif
 
 private:
-	
+	uint16_t lw_enc_counter,rw_enc_counter;
+	uint16_t _ar_width;
+	//функции обрабоки прерываний от энкодера колес
+	void encoderFuncM_L();
+	void encoderFuncM_R();
+
 };
 #endif
